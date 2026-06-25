67d1903291173a60c88952104c0bfe4b0da9e776
anurag-kkumar <anukumar95084@gmail.com>
2026-06-25 15:24:56 +0530

    Que 2

---
+++ Week1_AlgorithmsDataStructures_HandsOn/2_E-commerce_Platform_Search_Function/Code/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        Stored shop = new Stored();
+        long start, end;
+        int productId;
+
+
+        shop.storeProducts();
+
+
+
+        System.out.println("Linear Search :");
+        start = System.nanoTime();
+        productId = shop.linearSearch(4);
+        end = System.nanoTime();
+        shop.displayProduct(productId);
+        System.out.println("Time Taken : "+(end - start));
+
+
+
+
+        System.out.println("Binary Search :");
+        start = System.nanoTime();
+        productId = shop.binarySearch(4);
+        end = System.nanoTime();
+        shop.displayProduct(productId);
+        System.out.println("Time Taken : "+(end - start));
+    }
+}
