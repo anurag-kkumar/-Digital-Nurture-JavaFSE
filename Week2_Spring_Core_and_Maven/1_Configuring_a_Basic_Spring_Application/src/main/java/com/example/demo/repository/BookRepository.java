a524b4fc4aabc6a60cb6eae163fa31dd9fb20f32
anurag-kkumar <anukumar95084@gmail.com>
2026-07-06 00:19:35 +0530

    Ques1

---
+++ Week2_Spring_Core_and_Maven/1_Configuring_a_Basic_Spring_Application/src/main/java/com/example/demo/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+public class BookRepository {
+
+    public  BookRepository() {
+        System.out.println("This is BookRepository");
+    }
+}
---
+++ Week2_Spring_Core_and_Maven/1_Configuring_a_Basic_Spring_Application/src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+public class BookService {
+
+    public BookService() {
+        System.out.println("This is BookService");
+    }
+}
